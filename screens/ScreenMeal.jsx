a9a2bc58314e90e71c8e2ffbcf369a59f80a8e52
han8129 <110092300+han8129@users.noreply.github.com>
2024-03-21 17:18:33 +0700

    Change File Extension

--- screens/ScreenMeal.jsx
+++ screens/ScreenMeal.jsx
@@ -8,85 +8,86 @@ import Subtitle from "../components/MealDetail/Subtitle";
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
 
-function MealDetailScreen({ route, navigation }) {
-  const context = useContext(Context);
-  const mealId = route.params?.mealId;
+export default function ScreenMeal({ route, navigation }) {
+    const context = useContext(Context);
+    const mealId = route.params?.mealId;
 
     const [fav, setFav] = useState(context.favorites.includes(mealId));
 
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  function headerButtonPressHandler() {
-    console.log("Pressed!");
+    function headerButtonPressHandler() {
+        setFav((current) => {
+            let star;
+            if (current == true) {
+                context.removeFavorite(mealId);
+                star = false;
+            } else {
+                context.addFavorite(mealId);
+                star = true;
+            }
 
-    setFav((current) => {
-        let star;
-        if ( current == true ) {
-            context.removeFavorite(mealId);
-            star = false;
-        } else {
-            context.addFavorite(mealId);
-            star = true;
-        }
+            return star;
+        });
 
-      return star;
-    });
-  }
+        navigation.setOptions({
+            headerRight: () => (
+                <IconButton
+                    icon={fav ? "heart" : "heart-outline"}
+                    color="white"
+                    onPress={headerButtonPressHandler}
+                />
+            ),
+        });
+    }
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => {
-        return (
-          <IconButton
-            icon={fav ? "heart" : "heart-outline"}
-            color="white"
-            onPress={headerButtonPressHandler}
-          />
-        );
-      },
-    });
-  }, [fav]);
-
-  return (
-    <ScrollView style={styles.rootContainer}>
-      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
-      <Text style={styles.title}>{selectedMeal.title}</Text>
-      <MealDetails
-        duration={selectedMeal.duration}
-        complexity={selectedMeal.complexity}
-        affordability={selectedMeal.affordability}
-        textStyle={styles.detailText}
-      />
-      <View style={styles.listOuterContainer}>
-        <View style={styles.listContainer}>
-          <Subtitle>Ingredients</Subtitle>
-          <List data={selectedMeal.ingredients} />
-          <Subtitle>Steps</Subtitle>
-          <List data={selectedMeal.steps} />
-        </View>
-      </View>
-    </ScrollView>
-  );
+    return (
+        <ScrollView style={styles.rootContainer}>
+            <Image
+                style={styles.image}
+                source={{ uri: selectedMeal.imageUrl }}
+            />
+            <Text style={styles.title}>{selectedMeal.title}</Text>
+            <MealDetails
+                duration={selectedMeal.duration}
+                complexity={selectedMeal.complexity}
+                affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
+            />
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data={selectedMeal.ingredients} />
+                    <Subtitle>Steps</Subtitle>
+                    <List data={selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
+    );
 }
 
-export default MealDetailScreen;
-
-    height: 350,
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 24,
-    margin: 8,
-    textAlign: "center",
-    color: "white",
-  },
-  detailText: {
-    color: "white",
-  },
-  listOuterContainer: {
-    alignItems: "center",
-  },
-  listContainer: {
-    width: "80%",
-  },
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32,
+    },
+    image: {
+        width: "100%",
+        height: 350,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 24,
+        margin: 8,
+        textAlign: "center",
+        color: "white",
+    },
+    detailText: {
+        color: "white",
+    },
+    listOuterContainer: {
+        alignItems: "center",
+    },
+    listContainer: {
+        width: "80%",
+    },
 });
